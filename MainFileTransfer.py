5c9b5bf9c62efd01d66d6c48c2324a5b5c1129ed
Anuraj Pilanku <110219574+AnurajPilanku@users.noreply.github.com>
2023-01-03 19:31:32 +0530

    Update MainFileTransfer.py

--- MainFileTransfer.py
+++ MainFileTransfer.py
@@ -8,17 +8,20 @@ import os
 import shutil
 import sys
 
+
 # downloads=r"C:\Users\2040664\anuraj\lcs_borker"
 # targetfolder=r"C:\Users\2040664\anuraj\ERP"
 #file_names=['t2','t2','t2','vdgeef']
 def mainDocFileTransfer(file_names,foldername,targetfolder,downloads):
     '''______Create Directories in target Folder________'''
     mainfolder=os.path.join(targetfolder,foldername)
-    os.mkdir(mainfolder)
-    os.mkdir(os.path.join(mainfolder,"Master_Agreements"))
+    if os.path.exists(mainfolder)==False:
+        os.mkdir(mainfolder)
+    if os.path.exists(os.path.join(mainfolder,"Master_Agreements"))==False:
+        os.mkdir(os.path.join(mainfolder,"Master_Agreements"))
 
     '''______Create Versioning of the dupliacte files________'''
-    extenChange=lambda x:x.replace(".docx","").replace(".txt","").replace(".csv","").replace(".pdf","").replace(".xlsx","").replace(".py","").replace(".docs","")
+    extenChange=lambda x:x.replace(".docx","").replace(".txt","").replace(".csv","").replace(".pdf","").replace(".xlsx","").replace(".py","").replace(".docs","").replace(".doc","").replace(".html","").replace(".json","").replace("(","").replace(")","").replace(".xlsm","").replace(".yaml","").replace(".csv","").replace(".csv","").replace(".PDF","")    
     filenames=list(map(extenChange,file_names))
     if type(filenames) in ['str',str]:
         print(1111)
@@ -42,9 +45,13 @@ def mainDocFileTransfer(file_names,foldername,targetfolder,downloads):
     for filename in Allfiles:
         for dirfile in fileNamesinDownloads:
             if filename in dirfile:
-                shutil.copy(os.path.join(downloads, dirfile), os.path.join(mainfolder, "Master_Agreements"))
-                os.remove(os.path.join(downloads, dirfile))
-                MainFileslist.append(dirfile)
+                if os.path.exists(os.path.join(downloads, dirfile)):
+                    try:
+                        shutil.copy(os.path.join(downloads, dirfile), os.path.join(mainfolder, "Master_Agreements"))
+                        os.remove(os.path.join(downloads, dirfile))
+                        MainFileslist.append(dirfile)
+                    except Exception as e:
+                        MainFileslist.append(dirfile+" -error : "+str(e))
     return  {"Downloaded Files Names : ":MainFileslist}
 #print(mainDocFileTransfer(file_names,sys.argv[1]))
 
